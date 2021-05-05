a2b7083ff5709d2fa15f442404cbec64dc5ea9d4
Badger Assassin <beware@badgerkingdom.com.tr>
2021-05-05 15:01:22 -0400

    pass over errors

--- contracts/BadgerHarvestAllScriptLib.sol
+++ contracts/BadgerHarvestAllScriptLib.sol
@@ -6,7 +6,7 @@ import { IStrategy } from "./IStrategy.sol";
 library BadgerHarvestAllScriptLib {
   function harvestAll(address[] memory strategies) internal {
     for (uint256 i = 0; i < strategies.length; i++) {
-      IStrategy(strategies[i]).harvest();
+      strategies[i].call(abi.encodeWithSelector(IStrategy.harvest.selector));
     }
   }
 }
